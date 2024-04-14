d9837aa3aad5382d45437a3240f0303ccd03d585
Bence Nagy <bence@veldex.at>
2024-04-14 13:31:25 +0200

    add 0.0.0.0 to app.listen function in index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,8 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/users", userRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 AppDataSource.initialize();
